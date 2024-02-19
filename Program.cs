52f71cbd94885ee578ac4f01b18062dfa0ce615c
Luan1006 <thenoobsisbest@gmail.com>
2024-02-19 14:54:35 +0100

    Added Task2 in Program.cs

--- Program.cs
+++ Program.cs
@@ -19,10 +19,10 @@ HttpUtils httpUtils = HttpUtils.instance;
 Response startRespons = await httpUtils.Get(baseURL + startEndpoint + myPersonalID);
 Console.WriteLine($"Start:\n{Colors.Magenta}{startRespons}{ANSICodes.Reset}\n\n");
 string startResponsContent = startRespons.content;
-Task task = new Task(startResponsContent);
+Task startTask1 = new Task(startResponsContent);
 
 //#### FIRST TASK 
-Response task1Response = await httpUtils.Get(baseURL + taskEndpoint + myPersonalID + "/" + task.taskID); // Get the task from the server
+Response task1Response = await httpUtils.Get(baseURL + taskEndpoint + myPersonalID + "/" + startTask1.taskID); // Get the task from the server
 string task1ResponseContent = task1Response.content;
 Task task1 = new Task(task1ResponseContent);
 Console.WriteLine($"Task 1:\n{Colors.Magenta}{task1.title}\n{task1.description}{ANSICodes.Reset}");
@@ -32,6 +32,12 @@ float celsius = (fahrenheit - 32) * 5 / 9;
 celsius = (float) Math.Round(celsius, 2);
 string task1Answer = celsius.ToString();
 Console.WriteLine($"Temperature in celsius: {Colors.Green}{celsius}{ANSICodes.Reset}\n\n");
-
 Response task1SubmitResponse = await httpUtils.Post(baseURL + taskEndpoint + myPersonalID + "/" + task1.taskID, task1Answer);
 Console.WriteLine($"Answer: {Colors.Green}{task1SubmitResponse}{ANSICodes.Reset}");
+Task startTask2 = new Task(task1SubmitResponse.content);
+
+//#### SECOND TASK
+Response task2Response = await httpUtils.Get(baseURL + taskEndpoint + myPersonalID + "/" + startTask2.taskID); // Get the task from the server
+string task2ResponseContent = task2Response.content;
+Task task2 = new Task(task2ResponseContent);
+Console.WriteLine($"Task 2:\n{Colors.Magenta}{task2.title}\n{task2.description}{ANSICodes.Reset}");
