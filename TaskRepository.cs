de1f3d27c49a2aeb918f20e4224001486c118daa
Luan1006 <thenoobsisbest@gmail.com>
2024-02-20 00:43:03 +0100

    Updated TaskRepository and added Consttants.cs

---
+++ TaskRepository.cs
@@ -0,0 +1,12 @@
+using HTTPUtils;
+using System.Text.Json;
+using AnsiTools;
+using Colors = AnsiTools.ANSICodes.Colors;
+
+public class TaskRepository
+{
+    public class Task1
+    {
+        
+    }
+}
