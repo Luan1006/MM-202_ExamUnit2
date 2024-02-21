697756e1603cbd01ebcff1ba9def45d84b41d08e
Luan1006 <thenoobsisbest@gmail.com>
2024-02-21 15:37:10 +0100

    Further refactored HttpUtils.cs

--- HttpUtils.cs
+++ HttpUtils.cs
@@ -43,6 +43,13 @@ namespace HTTPUtils
 
         }
 
+        #region Helper Methods
+
+        private Response CreateResponse(string respons, int responseStatusCode, string url)
+        {
+            return new Response() { content = respons, statusCode = responseStatusCode, url = url };
+        }
+
         private Response HandleHttpRequestException(HttpRequestException e, string url)
         {
             int statusCode = (int)(e.StatusCode ?? 0);
@@ -52,9 +59,14 @@ namespace HTTPUtils
             return new Response() { content = null, statusCode = statusCode, url = url };
         }
 
-        private Response CreateResponse(string respons, int responseStatusCode, string url)
+        #endregion
+
+        #region Get Methods
+        private async Task<Response> CreateResponseFromHttpResponse(HttpResponseMessage response, string url)
         {
-            return new Response() { content = respons, statusCode = responseStatusCode, url = url };
+            string content = await response.Content.ReadAsStringAsync();
+            int statusCode = (int)response.StatusCode;
+            return CreateResponse(content, statusCode, url);
         }
 
         public async Task<Response> Get(string url)
@@ -62,33 +74,45 @@ namespace HTTPUtils
             return await SendGetRequest(url);
         }
 
-        public async Task<Response> Post(string url, string content)
-        {
-            return await SendPostRequest(url, content);
-        }
-
         private async Task<Response> SendGetRequest(string url)
         {
             try
             {
                 HttpResponseMessage response = await httpClient.GetAsync(url);
-                string respons = await response.Content.ReadAsStringAsync();
-
-                return CreateResponse(respons, (int)response.StatusCode, url);
+                return await CreateResponseFromHttpResponse(response, url);
             }
             catch (HttpRequestException e)
             {
                 return HandleHttpRequestException(e, url);
             }
+        }
+
+        #endregion
+
+        #region Post Methods
+
+        private Answer CreateAnswer(string content)
+        {
+            return new Answer() { answer = content };
+        }
+
+        private async Task<HttpResponseMessage> SendRequest(string url, Answer answer)
+        {
+            return await httpClient.PostAsJsonAsync(url, answer);
+        }
+
+        private async Task<string> ReadResponseContent(HttpResponseMessage response)
+        {
+            return await response.Content.ReadAsStringAsync();
         }
 
         private async Task<Response> SendPostRequest(string url, string content)
         {
             try
             {
-                Answer answer = new Answer() { answer = content };
-                HttpResponseMessage response = await httpClient.PostAsJsonAsync(url, answer);
-                string respons = await response.Content.ReadAsStringAsync();
+                Answer answer = CreateAnswer(content);
+                HttpResponseMessage response = await SendRequest(url, answer);
+                string respons = await ReadResponseContent(response);
 
                 return CreateResponse(respons, (int)response.StatusCode, url);
             }
@@ -97,6 +121,14 @@ namespace HTTPUtils
                 return HandleHttpRequestException(e, url);
             }
         }
+
+        public async Task<Response> Post(string url, string content)
+        {
+            return await SendPostRequest(url, content);
+        }
+
+
+        #endregion
     }
 
     class Answer
