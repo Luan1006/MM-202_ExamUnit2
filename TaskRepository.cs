04400bc7109337d6f20b33fd667e3d6e14900f82
Luan1006 <thenoobsisbest@gmail.com>
2024-02-23 18:13:32 +0100

    Moved Series class to Series.cs

--- TaskRepository.cs
+++ TaskRepository.cs
@@ -6,44 +6,5 @@ public class TaskRepository
 
     
 
-    public class Series
-    {
-        public static string Run(Task task)
-        {
-            int[] series = ParseParameters(task.parameters);
-            int answer = CalculateNextInSeries(series);
 
-            Print.PrintTaskDetails(Text.TaskFour, task, answer.ToString());
-
-            return answer.ToString();
-        }
-
-        public static int[] ParseParameters(string parameters)
-        {
-            int[] series;
-            try
-            {
-                series = parameters.Split(Text.CharComma).Select(int.Parse).ToArray();
-            }
-            catch (FormatException)
-            {
-                series = [];
-            }
-
-            return series;
-        }
-
-        public static int CalculateNextInSeries(int[] series)
-        {
-            if (series.Length < 2)
-            {
-                return 0;
-            }
-
-            int lastInSeries = series[series.Length - 1];
-            int secondLastInSeries = series[series.Length - 2];
-
-            return lastInSeries * 2 - secondLastInSeries;
-        }
-    }
 }
--- Tasks/Series.cs
+++ Tasks/Series.cs
@@ -2,5 +2,44 @@ using static Constants;
 
 namespace TaskRepository
 {
-    
+    public class Series
+    {
+        public static string Run(Task task)
+        {
+            int[] series = ParseParameters(task.parameters);
+            int answer = CalculateNextInSeries(series);
+
+            Print.PrintTaskDetails(Text.TaskFour, task, answer.ToString());
+
+            return answer.ToString();
+        }
+
+        public static int[] ParseParameters(string parameters)
+        {
+            int[] series;
+            try
+            {
+                series = parameters.Split(Text.CharComma).Select(int.Parse).ToArray();
+            }
+            catch (FormatException)
+            {
+                series = [];
+            }
+
+            return series;
+        }
+
+        public static int CalculateNextInSeries(int[] series)
+        {
+            if (series.Length < 2)
+            {
+                return 0;
+            }
+
+            int lastInSeries = series[series.Length - 1];
+            int secondLastInSeries = series[series.Length - 2];
+
+            return lastInSeries * 2 - secondLastInSeries;
+        }
+    }
 }
